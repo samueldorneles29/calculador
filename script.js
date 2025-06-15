d1343565f73e880eb73b72b3030ee24d598d2b57
samueldorneles29 <samuel.dorneles@escola.pr.gov.br>
2025-06-15 11:58:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function soma() {
+    let numero_um = parseInt(document.getElementById('primeiro_numero').value);
+    let numero_dois = parseInt(document.getElementById('segundo_numero').value);
+    let soma_numero = numero_um + numero_dois;
+
+    document.getElementById('resultado').textContent = 'Resultado: ' + soma_numero;
+}
+
+function subtrair() {
+    let numero_um = parseInt(document.getElementById('primeiro_numero').value);
+    let numero_dois = parseInt(document.getElementById('segundo_numero').value);
+    let subtrai_numero = numero_um - numero_dois;
+
+    document.getElementById('resultado').textContent = 'Resultado: ' + subtrai_numero;
+}
